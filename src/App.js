2e005b1f0f17ee99d1b37b9b6c69ea83f1efaf7f
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2019-05-13 15:04:38 -0400

    Added aphrodite for css, started adding stylesheets

--- src/App.js
+++ src/App.js
@@ -1,39 +1,81 @@
 import React, { Component } from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom'
+import { StyleSheet, css } from 'aphrodite'
 
 import Home from './Home'
 import Projects from './Projects'
 import Contact from './Contact'
 
-import './App.css'
+
+class App extends Component{
+  state = {
+    curpath: "/",
+  }
 
 
-class App extends React.Component{
 
   render(){
     return(
-      <div className="App">
-        <ul className="Links">
-          <li>
-            <NavLink to="/">Home</NavLink>
+      <div className={css(styles.App)} >
+        <ul className={css(styles.list)} >
+          <li style={styles.listItem}>
+            <NavLink to="/"  >Home</NavLink>
           </li>
-          <li>
-            <NavLink to="/projects">Projects</NavLink>
+          <li style={styles.listItem}>
+            <NavLink to="/projects" >Projects</NavLink>
           </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
+          <li style={styles.listItem}>
+            <NavLink to="/contact" >Contact</NavLink>
           </li>
         </ul>
+
+        
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/projects" component={Projects}/>
           <Route path="/contact" component={Contact}/>
         </Switch>
+
+        <ul className={css(styles.list)} >
+          <li style={styles.listItem}>
+              <NavLink to="/" >Home</NavLink>
+            </li>
+            <li style={styles.listItem}>
+              <NavLink to="/projects" >Projects</NavLink>
+            </li>
+            <li style={styles.listItem}>
+              <NavLink to="/contact" >Contact</NavLink>
+            </li>
+        </ul>
+
+
       </div>
     )
   }
-
 }
 
+const styles = StyleSheet.create({
+  App:{
+    textAlign: 'center',
+
+  },
+  list:{
+    listStyleType: 'none',
+    margin: 0,
+    backgroundColor:'#333'
+  },
+  listItem:{
+    float: 'center',
+
+  },
+  link:{
+    display:'block',
+    color:'white',
+    textAlign:'center',
+    padding: '14px 16px',
+    textDecoration: 'none',
+
+  }
+})
 
 export default App;
