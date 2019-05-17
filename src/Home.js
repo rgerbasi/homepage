18e007a4e7bdf843a4eebb47cdabcb6bf5dfb98f
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2019-05-17 13:53:40 -0400

    Added containers to the home page

--- src/Home.js
+++ src/Home.js
@@ -12,59 +12,71 @@ class Home extends Component {
         
         return(
             <div className="home" >
-                
+                <br/>
                 <Container>
-                <p>
-                    Individual studying Computer Science with a concentration in Computer Graphics who is also a cheerleader at Purdue University.
-                </p>
+                <Row>
+                    <Col>Individual studying Computer Science with a concentration in Computer Graphics who is also a cheerleader at Purdue University.</Col>
+                </Row>
                 </Container>
-               
-                <Container>
-                    
-                    <div className="classes-taken">Computer Science courses taken at Purdue
-                    <ul>
-                        <li>
-                            CS 180: Problem Solving and Object-Oriented Programming (Java)
-                        </li>
-                        <li>
-                            CS 182: Foundations of Computer Science (Discrete Math)
-                        </li>
-                        <li>
-                            CS 240: Programming in C
-                        </li>
-                        <li>
-                            CS 250: Computer Architecture
-                        </li>
-                        </ul>
-                    </div>
-                </Container>
-               
-                <Container>
-                    <div className="classes-inprogress">Classes in progress
-                    <ul>
-                        <li>
-                            Stat 350: Intro to Statistics
-                        </li>
-                        <li>
-                            CS 251: Data Structures And Algorithms
-                        </li>
-                        <li>
-                            MA 265: Linear Algebra
-                        </li>
-                        <li>
-                            PSY 120: Elementary Psychology
-                        </li>
-                        <li>
-                            COM 217: Science Writing and Presentation
-                        </li>
-                    </ul>
-                    </div>
-                </Container>
-               
+                <br/>
 
+                <Container>
+                    <Row>
+                        <Col><h1>
+                            Computer Science courses taken
+                        </h1></Col>
+
+                    </Row>
+                    <div className="classes-taken">
+                    
+                        <Row>
+                            <Col>CS 180: Problem Solving and Object-Oriented Programming (Java)</Col>
+                        </Row>
+                        <Row>
+                            <Col>CS 182: Foundations of Computer Science (Discrete Math)</Col>
+                        </Row>
+                        <Row>
+                            <Col>CS 240: Programming in C</Col>
+                        </Row>
+                        <Row>
+                            <Col>CS 250: Computer Architecture</Col>
+                        </Row>
+                        
+                    </div>
+                </Container>
+
+                <Container>
+                    <Row>
+                        <Col></Col><h1>
+                            Classes in progress
+                        </h1>
+                    </Row>
+                    <div className="classes-inprogress">
+                        <Row>
+                            <Col></Col>Stat 350: Intro to Statistics
+                        </Row>
+                        <Row>
+                            <Col></Col>CS 251: Data Structures And Algorithms
+                        </Row>
+                        <Row>
+                            <Col></Col>MA 265: Rownear Algebra
+                        </Row>
+                        <Row>
+                            <Col></Col> PSY 120: Elementary Psychology
+                        </Row>
+                        <Row>
+                            <Col></Col>COM 217: Science Writing and Presentation
+                        </Row>
+                    
+                    </div>
+                </Container>
+                <br/>
+                <Container>
+                
                 <div className="cheer">
-                    Cheerstuff
+                Cheerstuff
                 </div>
+                </Container>
                 
 
             </div>
